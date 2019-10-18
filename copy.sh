c3e6835b84573f18a3d87aa2c46833374a2678b0
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2019-10-18 11:19:06 +0200

    CHG Message mechanism in copy

--- copy.sh
+++ copy.sh
@@ -1,47 +1,84 @@
 #!/bin/bash
 
 exec_dir=$(dirname $0)
-
-status="$exec_dir/status.sh"
-if [ "$?" -ne "0" ] || [ ! -x "$status" ]; then
-    msg="Kann den Status nicht speichern";
-    notify-send -u critical "Backup" "$msg"
-    if [ -w $exec_dir ]; then
-        fatal_log=$exec_dir/run_error
-    else
-        fatal_log=$HOME/fatal_backup_error
-    fi
-    echo $(date) $msg | tee -a $fatal_log >&2
+if [ "$(which notify-send)" == "" ]; then
+    echo "Kann notify-send nicht finden, bitte installieren";
     exit 127
 fi
 
+function notify {
+    msg=""
+    level=$1
+    shift
+    case "$level" in
+        FAIL) # Fehler beim Ausführen
+            subject="FAIL"
+            urgency="critical"
+            head="Backup: Fehler $(date)"
+        ;;
+        FATAL) # Fehler im Script / Konfiguration
+            subject="WTF!"
+            urgency="critical"
+            head="Backup-Fehler (Fatal)"
+        ;;
+        START)
+            subject="START"
+            urgency="low"
+            head="Starte Backup"
+            msg="$(date)"
+        ;;
+        SUCCESS)
+            subject="SUCC"
+            urgency="low"
+            head="Backup: Erfolg"
+        *)
+            subject="$1"
+            shift
+        ;;
+    esac
+    msg=$* $msg
+    notify-send -u $urgency "$head" "$msg"
+    logcmd=$($config --section notify --key log)
+    logpipe=$($config --section notify --key pipe)
+    logmsg="$(date) $level_for_log $message"
+    if [ "$logpipe" == "yes" ]; then
+        echo "$logmsg" | $logcmd
+    else
+        $logcmd "$logmsg"
+    fi
+}
+
 config_dir="$($exec_dir/config_dir)"
 if [ "$?" -ne "0" ] || [ -z "$config_dir" ]; then
-    $status FATAL "Kann Konfigurationsverzeichnis nicht bestimmen"
+    notify FATAL "Kann Konfigurationsverzeichnis nicht bestimmen"
     exit 127;
 fi
 config="$exec_dir/config.py --path $config_dir"
+if ! $config --help > /dev/null 2>&1; then
+    notify FATAL "Konfigurationsscript kann nicht gestartet werden"
+    exit 127;
+fi
 
-$status START ""
+notify START "Starte Backup" "$(date)"
 
 # This part should do the actual copying.
 source=$($config --section source --key path)
 if [ "$?" -ne "0" ] || [ -z "$source" ]; then
-    $status FATAL "Kann Quellpfad nicht bestimmen\n(sollte in source::path stehen)"
+    notify FATAL "Kann Quellpfad nicht bestimmen (sollte in source::path stehen)"
     exit 127;
 fi
 target=$($config --section target --key path)
 if [ "$?" -ne "0" ] || [ -z "$target" ]; then
-    $status FATAL "Kann Zielpfad nicht bestimmen\n(sollte in target::path stehen)"
+    notify FATAL "Kann Zielpfad nicht bestimmen (sollte in target::path stehen)"
     exit 127;
 fi
 exclude_file=$config_dir$($config --section source --key exclude_file)
 if [ "$?" -ne "0" ] || [ -z "$exclude_file" ]; then
-    $status FATAL "Kann nicht bestimmen, welche Dateien vom Backup ausgeschlossen werden sollen\n(sollte in source::exclude_file stehen)"
+    notify FATAL "Kann nicht bestimmen, welche Dateien vom Backup ausgeschlossen werden sollen (sollte in source::exclude_file stehen)"
     exit 127;
 fi
 if [ ! -r "$exclude_file" ]; then
-    $status FATAL "Kann Datei mit Ausschlussliste nicht lesen\n$exclude_file"
+    notify FATAL "Kann Datei mit Ausschlussliste nicht lesen\n$exclude_file"
     exit 127;
 fi
 
@@ -52,7 +89,7 @@ if [ ! -d $target ]; then
         $provide_exe
     fi
     if [ ! -d $target ]; then
-        $status FAIL "Ziel $target nicht gefunden, konnte auch nicht bereit gestellt werden"
+        notify FAIL "Ziel $target nicht gefunden, konnte auch nicht bereit gestellt werden"
         exit 2
     fi
 fi
@@ -68,20 +105,21 @@ rsync_error=$?
 
 case $rsync_error in
     0)
-        $status SUCCESS "Backup erfolgreich ausgeführt"
+        notify SUCCESS
         exit 0;;
     23|24)
-        $status SUCCESS_MESSAGE "Nicht alle Quelldateien konnten gelesen werden";
-        exit 16;;
+        notify SUCCESS "Nicht alle Quelldateien konnten gelesen werden";
+        exit 0;;
     [1246])
-        $status FAIL "rsync usage error";
+        notify FATAL "rsync usage error";
         exit 1;;
     3|5|10|11|12|13|14|21|22)
-        $status FAIL "rsync copy error ($rsync_error)";
+        notify FAIL "rsync copy error ($rsync_error)";
         exit 2;;
     25|40|35)
-        $status FAIL "rsync other error $rsync_error";
+        notify FAIL "rsync other error $rsync_error";
         exit 3;;
     *)
-        $status FAIL "Unknown rsync error $rsync_error";
+        notify FAIL "Unknown rsync error $rsync_error";
+        exit $rsync_error;;
 esac
