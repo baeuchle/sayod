cc57f85bd10a5e21023522939dab5ae43dd6edb3
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2019-10-23 13:46:44 +0200

    CHG improved config.py standalone

--- config.py
+++ config.py
@@ -12,20 +12,23 @@ def get_config(config_file):
     ini_obj.read(config_file)
     return ini_obj
 
-def find_entry(args):
-    configuration = get_config(args.file)
+def find_entry(filename, section, key, doesfileexist=False, default=None):
+    configuration = get_config(filename)
     if configuration is None:
         return 127, None
-    if args.doesfileexist:
+    if doesfileexist:
         return 0, None
-    if configuration.has_option(args.section, args.key):
-        return 0, configuration[args.section][args.key]
-    if args.emptydefault:
-        return 0, ""
-    if args.default is not None:
-        return 0, args.default
-    print("entry {}::{} not found".format(args.section, args.key), file=sys.stderr)
+    if configuration.has_option(section, key):
+        return 0, configuration[section][key]
+    if default is not None:
+        return 0, default
+    print("entry {}::{} not found".format(section, key), file=sys.stderr)
     return 1, None
+
+def find_entry_args(args):
+    if args.emptydefault:
+        args.default = ""
+    return find_entry(args.file, args.section, args.key, args.doesfileexist, args.default)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Returns configuration parameters')
@@ -36,7 +39,7 @@ if __name__ == "__main__":
     parser.add_argument('--default', required=False, help='Return this string if no entry found')
     parser.add_argument('--doesfileexist', required=False, default=False, action='store_true', help='Exit 0 if config file exists, 127 otherwise.')
     args = parser.parse_args()
-    status, data = find_entry(args)
+    status, data = find_entry_args(args)
     if data is not None:
         print(data)
     sys.exit(status)
