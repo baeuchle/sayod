7d4f8d26faba965d152bd040b3b0251a62b828ff
Bjørn Bäuchle <baeuchle@frankfurtium.de>
2024-10-07 17:18:54 +0200

    CHG receiver STILL exe, also module
    Moved relevant code to new module 'receive', but an exe remains as
    end-point for legacy code

---
+++ receive.py
@@ -0,0 +1,45 @@
+import logging
+import sys
+
+from config import Config
+from taggedlog import TaggedLog, FromStream
+
+rlog = logging.getLogger('backup.receiver')
+
+class _Receiver:
+    def __init__(self, content_type):
+        self.content_type = content_type
+
+    def run(self):
+        status_file = Config.get().find('status', 'file', None)
+        if status_file is None:
+            rlog.warning("Status file cannot be determined")
+            raise SystemExit(1)
+        log_obj = TaggedLog(status_file, 'a+')
+        entry = FromStream(sys.stdin, self.content_type)
+        log_obj.append(entry)
+
+class Receiver:
+    _instance = None
+
+    @classmethod
+    def add_subparser(cls, sp):
+        _ = sp.add_parser('receive',
+            help='''Reads new log entries from STDIN and adds them to the appropriate log.
+            Communication follows text/x-plain-log type''')
+
+    @classmethod
+    def standalone(cls, _):
+        if _instance is None:
+            _instance = _Receiver('text/x-plain-log')
+        _instance.run()
+
+    @classmethod
+    def init_from_stdin(cls):
+        line = sys.stdin.readline().strip()
+        content_type = "text/x-plain-log"
+        if line.startswith("content-type: "):
+            content_type = line[len("content-type: "):]
+            line = sys.stdin.readline().strip()
+        Config.init_file(line)
+        _instance = _Receiver(content_type)
